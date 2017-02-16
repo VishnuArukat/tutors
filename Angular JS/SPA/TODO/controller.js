87966e7d3d7fe095a893ad17054d2dbc809857e3
VishnuArukat <vishnuaa007@gmail.com>
2017-02-16 17:41:29 +0530

    todo app status and edit

--- Angular JS/SPA/TODO/controller.js
+++ Angular JS/SPA/TODO/controller.js
@@ -4,22 +4,44 @@ app.controller('todo', ['$scope', function($scope){
 
 	$scope.tasks = [];
 
-	$scope.searchEnter = function(){
+	$scope.searchEnter = function(){ 
 		
 	// defining the function in the $scope
 
 		$scope.addTask = function(){
-			$scope.tasks.push($scope.task_name);
-			console.log($scope.task)
+			// making the task as read or not read
+			$scope.tasks.push({'task_text':$scope.task_name,'status':false});
+			console.log($scope.task_name)
+			console.log($scope.tasks)
 		}
 
 
 		// checking which key is pressed enter keycode is 13
 
 		console.log(event.which || event.keyCode);
-		if (event.which ==13){
+		if (event.which == 13 && $scope.task_name != ''){
+
 			// calling a function for adding the task below
 			$scope.addTask();
+			// clear the input field after the  push
+			$scope.task_name = ''
 		}
+		$scope.contentEdit = function(){
+ 
+			if(event.target.contentEditable =='false'){
+				event.target.contentEditable ="true";
+			}else{
+				event.target.contentEditable ="false";
+			}
+		}
+
+		 
+		$scope.doneEnter=function(){
+			if (event.which == 13 && $scope.task_text != ''){		
+			$scope.contentEdit();
+		}
+		};
+
+		 
 	}
 }])
